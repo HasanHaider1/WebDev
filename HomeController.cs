4981cef4c887a76ee11114771fa41b8ad6704c6b
Hasan Haider <31242825+HasanHaider1@users.noreply.github.com>
2019-12-14 16:03:06 +0500

    Controller for charts

--- HomeController.cs
+++ HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using WebDevProject.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace WebDevProject.Controllers
 {
@@ -10,15 +14,18 @@ namespace WebDevProject.Controllers
     {
         public ActionResult Index()
         {
+            List<DataPoint> dataPoints = new List<DataPoint>{
+                new DataPoint(10, 22),
+                new DataPoint(20, 36),
+                new DataPoint(30, 42),
+                new DataPoint(40, 51),
+                new DataPoint(50, 46),
+            };
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
 
         public ActionResult Contact()
         {
@@ -26,23 +33,40 @@ namespace WebDevProject.Controllers
 
             return View();
         }
+        public ActionResult CareTaker()
+        {
+            ViewBag.Message = "Your CareTaker page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Organization,Admin")]
         public ActionResult Organization()
         {
             ViewBag.Message = "Roles.";
 
             return View();
         }
+
+        [Authorize(Roles = "Admin")]
         public ActionResult Admin()
         {
-            ViewBag.Message = "Roles.";
+            {
+                List<DataPoint> dataPoints = new List<DataPoint>{
+                new DataPoint(10, 22),
+                new DataPoint(20, 36),
+                new DataPoint(30, 42),
+                new DataPoint(40, 51),
+                new DataPoint(50, 46),
+            };
 
-            return View();
-        }
-        public ActionResult CareTaker()
-        {
-            ViewBag.Message = "Roles.";
+                ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+                ViewBag.Message = "Roles.";
 
-            return View();
+                return View();
+            }
+
+
         }
     }
 }
